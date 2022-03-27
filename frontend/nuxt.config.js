3dc37d28332fe66b82c08bcd770fd785f26cda7f
vertesip <hello@vertesipatrik.com>
2022-03-27 12:01:26 +0200

    Fixed nuxt dev server and mounting in docker

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -42,5 +42,11 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    loaders:  {
+      vue: {
+         prettify: false
+      }
+    }
+  },
 }
