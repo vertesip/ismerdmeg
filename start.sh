f87f79eeaadf81653a92101f88abcae93e05ba7b
vertesip <hello@vertesipatrik.com>
2022-03-28 20:48:03 +0200

    Added nuxt install to container

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker-compose up -d
-winpty docker exec -it frontend //bin//sh -c "npm install && npm run dev"
+winpty docker exec -it frontend //bin//sh -c "npm install nuxt && npm run dev"
