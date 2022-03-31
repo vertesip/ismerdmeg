e282c41225b19d53277e90949fb69fc31892800e
vertesip <hello@vertsipatrik.com>
2022-03-31 20:58:28 +0200

    Fixed frontend and backend startup, added graphql

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+docker-compose build
 docker-compose up -d
-winpty docker exec -it frontend //bin//sh -c "npm install nuxt && npm run dev"
-#winpty docker exec -it backend //bin//sh -c "npm install @nuxtjs/apollo graphql graphql-tag"
+
+docker exec -it frontend //bin//sh -c "npm install --dev && npm install nuxt && npm run dev"
