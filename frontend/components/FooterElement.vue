989c34cbf8a761004be572228cebc3de99f5a10e
vertesip <hello@vertsipatrik.com>
2022-04-13 16:23:39 +0200

    Added basic footer

---
+++ frontend/components/FooterElement.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Ismerdmeg</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+footer{
+    margin-top: auto;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,18 +2,47 @@
   <div>
     <NavBar />
     <GoogleMaps />
+    <FooterElement />
   </div>
 </template>
 
 <script>
 import NavBar from '../components/NavBar'
 import GoogleMaps from '../components/GoogleMaps'
+import FooterElement from '../components/FooterElement'
 
 export default {
   name: 'IndexPage',
   components: {
     NavBar,
-    GoogleMaps
+    GoogleMaps,
+    FooterElement
   }
 }
 </script>
+
+<style scoped>
+body{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+#__nuxt{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+#__layout{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+#__layout > div{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+</style>
