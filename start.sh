7cc8188fc713d98390cc04bc69769e041122ac2d
Dávid Borsodi <corten.mgs@gmail.com>
2022-04-17 21:20:19 +0200

    feat: nginx working

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker compose build --no-cache
+docker compose build #--no-cache
 docker compose up -d
 
 docker exec -it nuxt //bin//sh -c "npm install --dev && npm install nuxt && npm run dev"
