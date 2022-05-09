c40cc4aceddbe8f1bc1428ea87ad3e7a1bf8fa6c
vertesip <hello@vertsipatrik.com>
2022-05-09 22:33:19 +0200

    Changed shell file

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 #docker compose build
 docker compose up -d
 
-docker exec -it nuxt //bin//sh -c "npm install --dev && npm install nuxt && npm run dev"
+docker exec -i nuxt //bin//sh -c "npm install --dev && npm install nuxt && npm run dev"
