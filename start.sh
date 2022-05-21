c214787702a8b33894bd4e1c593323fb8d049a38
vertesip <hello@vertesipatrik.com>
2022-05-21 20:54:04 +0200

    Updated start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ "${NODE_ENV}" = "development" ]; 
     then
     DOCKER_BUILDKIT=1 docker compose -f docker-compose.yml build --build-arg NODE_ENV=development
-    docker compose -f docker-compose.dev.yml up -d
+    docker compose -f docker-compose.yml up -d
     docker compose up -d
 
     exit;
@@ -11,7 +11,7 @@ fi
 if [ "${NODE_ENV}" = "production" ]; 
     then
     DOCKER_BUILDKIT=1 docker compose -f docker-compose.yml build --build-arg NODE_ENV=production
-    docker compose -f docker-compose.prod.yml up -d
+    docker compose -f docker-compose.yml up -d
     docker compose up -d
 
     exit;
