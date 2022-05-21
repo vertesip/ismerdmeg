573a377c16e565a30a322489866e321dda16e3ee
vertesip <hello@vertesipatrik.com>
2022-05-21 20:42:27 +0200

    Updated stop and deployment file

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker stop nuxt && docker stop strapi && docker stop nginx && docker stop meilisearch
+docker stop nuxt && docker stop strapi && docker stop nginx && docker stop meilisearch && docker stop strapi_events
